0a6224dc5cc3daf19cd80a9dd4b13d1ff9f79487
juhaer009 <juhaerjafid1998@gmail.com>
2025-10-21 18:10:47 +0600

    final commit

--- src/firbase/firebase.config.js
+++ src/firbase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-81CjJT781VJ4LKnE6-HfNFSwCfK-Ud0",
-  authDomain: "dragon-news-breaking-33464.firebaseapp.com",
-  projectId: "dragon-news-breaking-33464",
-  storageBucket: "dragon-news-breaking-33464.firebasestorage.app",
-  messagingSenderId: "125567599335",
-  appId: "1:125567599335:web:5ddbdea4eb598e1f2cd842"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
